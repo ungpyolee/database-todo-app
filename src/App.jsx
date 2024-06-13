29bff20f9c0e97aedcf648f93518effeb3de26bf
ozcodingschool <kdigital@nextrunners.co.kr>
2024-06-13 18:25:18 +0900

    불필요한 코드 제거

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TodoContainer from './components/TodoContainer';
 
 function App() {
-    const [theme, setTheme] = useState('light');
-
-    useEffect(() => {
-        const savedTheme = localStorage.getItem('theme');
-        if (savedTheme) {
-            setTheme(savedTheme);
-            document.documentElement.setAttribute('data-theme', savedTheme);
-        }
-    }, []);
-
-    const toggleTheme = () => {
-        const newTheme = theme === 'light' ? 'dark' : 'light';
-        setTheme(newTheme);
-        document.documentElement.setAttribute('data-theme', newTheme);
-        localStorage.setItem('theme', newTheme);
-    };
 
     return (
         <Router>
           <Routes>
-              <Route path="/" element={<TodoContainer theme={theme} toggleTheme={toggleTheme} />} />
+              <Route path="/" element={<TodoContainer theme={theme}/>} />
           </Routes>
         </Router>
     );
